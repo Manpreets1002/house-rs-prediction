a272e3c1d38c3f2d171cb52a6fa5494824f30a9e
Manpreets1002 <bhatia.manpreet1224@gmail.com>
2025-12-13 01:23:28 +0530

    made path universal

--- src/logger.py
+++ src/logger.py
@@ -4,9 +4,11 @@ from src.helper import TIMESTAMP
 
 LOG_PATH = f"{TIMESTAMP}.log"
 
-log_file_path = os.path.join("C:\\Users\\HP\\Github\\house-rs-prediction","logs")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+log_file_path = os.path.join(BASE_DIR,"logs")
 os.makedirs(log_file_path, exist_ok= True)
 
+print(log_file_path)
 LOG_FILE_PATH = os.path.join(log_file_path,LOG_PATH)
 
 logging.basicConfig(
